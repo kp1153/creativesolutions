4011093ce058f54dcac8a27414270eb53239babd
kp1153 <kamta_tiwari@yahoo.com>
2025-10-28 14:32:14 +0530

    force deploy

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -3,9 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { Card, CardHeader, CardTitle, CardContent } from "../../components/ui/card";
-import { Button } from "../../components/ui/button";
-
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
   const projects = [
