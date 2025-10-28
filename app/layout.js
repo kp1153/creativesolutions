6330f0b0ce4faef6d8539a69ffb8a68d50365002
kp1153 <kamta_tiwari@yahoo.com>
2025-10-28 14:43:00 +0530

    force deploy

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import Script from "next/script";
 
 const geistSans = Geist({
--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import HeroSection from "./components/HeroSection";
-import Visitors from "./components/Visitors";
-import TechStack from "./components/TechStack";
-import WorkProcess from "./components/WorkProcess";
-import LanguageExpertise from "./components/LanguageExpertise";
-import CallToAction from "./components/CallToAction";
+import HeroSection from "../components/HeroSection";
+import Visitors from "../components/Visitors";
+import TechStack from "../components/TechStack";
+import WorkProcess from "../components/WorkProcess";
+import LanguageExpertise from "../components/LanguageExpertise";
+import CallToAction from "../components/CallToAction";
 
 export default function Home() {
   return (
