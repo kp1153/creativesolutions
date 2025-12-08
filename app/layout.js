2cbe4f76ff1eebce7e95f620144c3151122da8c4
kp1153 <kamta_tiwari@yahoo.com>
2025-12-08 15:08:37 +0530

    force deploy

--- app/layout.js
+++ app/layout.js
@@ -258,6 +258,21 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
+        {/* Tawk.to Chat Widget */}
+        <Script id="tawk-to-script" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/693699c415212a1980343bbd/1jbukhaal';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
+
         <meta name="theme-color" content="#6366f1" />
         <meta name="format-detection" content="telephone=yes" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
